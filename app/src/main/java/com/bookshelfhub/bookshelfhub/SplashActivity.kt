285a9773af3c6f8b4de7ee5cca1acf67d731c83f
Bello Abraham <belloabraham101@gmail.com>
2021-05-07 08:09:50 -0400

    Initial commit

---
+++ app/src/main/java/com/bookshelfhub/bookshelfhub/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.bookshelfhub.bookshelfhub
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent= Intent(this, MainActivity::class.java);
+        startActivity(intent)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+       finish()
+    }
+
+}
