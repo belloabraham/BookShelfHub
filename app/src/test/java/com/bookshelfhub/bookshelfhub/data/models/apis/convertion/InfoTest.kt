b0c39230fe15a12e3ad6aa239a13a6b4a449b6d0
Bello Abraham <belloabraham101@gmail.com>
2022-07-07 08:35:13 -0500

    Wrote FixerTest.kt, QueryTest.kt and InfoTest.kt

--- app/src/test/java/com/bookshelfhub/bookshelfhub/data/models/apis/convertion/InfoTest.kt
+++ app/src/test/java/com/bookshelfhub/bookshelfhub/data/models/apis/convertion/InfoTest.kt
@@ -1,5 +1,39 @@
 package com.bookshelfhub.bookshelfhub.data.models.apis.convertion
 
-import org.junit.Assert.*
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
 
-class InfoTest
+class InfoTest{
+
+    private lateinit var info: Info
+
+    @Before
+    fun setUp() {
+        info = Info(1, 1.0)
+    }
+
+    @Test
+    fun classNameIsInfo(){
+        val info =  info::class.simpleName
+        assertThat(info).isEqualTo("Info")
+    }
+
+    @Test
+    fun contains_timestamp_Field(){
+        val timestamp =  info::class.members.find {
+            it.name == "timestamp"
+        }
+        assertThat(timestamp).isNotNull()
+    }
+
+    @Test
+    fun contains_rate_Field(){
+        val rate =  info::class.members.find {
+            it.name == "rate"
+        }
+        assertThat(rate).isNotNull()
+    }
+
+
+}
--- app/src/test/java/com/bookshelfhub/bookshelfhub/data/models/apis/convertion/QueryTest.kt
+++ app/src/test/java/com/bookshelfhub/bookshelfhub/data/models/apis/convertion/QueryTest.kt
@@ -1,5 +1,45 @@
 package com.bookshelfhub.bookshelfhub.data.models.apis.convertion
 
-import org.junit.Assert.*
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
 
-class QueryTest
+class QueryTest{
+    private lateinit var query: Query
+
+    @Before
+    fun setUp() {
+        query = Query("", "", 1)
+    }
+
+    @Test
+    fun classNameIsQuery(){
+        val query =  query::class.simpleName
+        assertThat(query).isEqualTo("Query")
+    }
+
+    @Test
+    fun contains_from_Field(){
+        val from =  query::class.members.find {
+            it.name == "from"
+        }
+        assertThat(from).isNotNull()
+    }
+
+    @Test
+    fun contains_to_Field(){
+        val to =  query::class.members.find {
+            it.name == "to"
+        }
+        assertThat(to).isNotNull()
+    }
+
+    @Test
+    fun contains_amount_Field(){
+        val amount =  query::class.members.find {
+            it.name == "amount"
+        }
+        assertThat(amount).isNotNull()
+    }
+
+}
